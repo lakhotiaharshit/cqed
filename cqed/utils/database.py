7f71300f77af5fda615c40c996e930813955e240
lakhotiaharshit <lakhotiaharshit@gmail.com>
2019-11-26 16:45:25 +0100

    Add function copytree

--- cqed/utils/database.py
+++ cqed/utils/database.py
@@ -3,6 +3,8 @@ This module is currently being worked upon. Please contact Harshit Lakhotia or
 Lukas Grünhaupt for more info.
 """
 from pathlib import Path
+import os
+import shutil
 
 from qcodes import initialise_or_create_database_at
 
@@ -21,3 +23,28 @@ def initialise_db_at(database_path, mirror_from_level=6):
     dest_dir = Path(default_path, *parts_curr_path[mirror_from_level:])
     dest_path = Path.joinpath(dest_dir, 'experiments.db ')
     initialise_or_create_database_at(dest_path)
+
+
+def ig_f(dir, files):
+    return [f for f in files if os.path.isfile(os.path.join(dir, f))]
+
+
+def copytree(src, dst, symlinks=False, ignore=ig_f, print_error=False):
+    """
+    TODO: Write doctstrings
+
+    Usage
+    >>> src = r'C:\Users\a-halakh\Documents\Microsoft\QcodesHarshit'
+    >>> dst = r'C:\Users\a-halakh\Music\QcodesData'
+    >>> copytree(src, dst)
+    """
+    for item in os.listdir(src):
+        s = os.path.join(src, item)
+        d = os.path.join(dst, item)
+        if os.path.isdir(s):
+            try:
+                shutil.copytree(s, d, symlinks, ignore)
+            except OSError as e:
+                if print_error:
+                    print(e)
+                copytree(s, d)
